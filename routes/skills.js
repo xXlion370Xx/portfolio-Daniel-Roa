3c3762918562afa81838915fd1943eb4b5329473
Daniel Roa <lion35528@gmail.com>
2022-10-19 18:11:51 -0500

    remove files unnecessaries

--- routes/skills.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* Response get */
-router.get('/', (req, res, next) => {
-    res.render('skills', { tittle: "Daniel Roa - Habilidades" });
-})
-
-module.exports = router;
