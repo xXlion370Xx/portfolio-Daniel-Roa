629eebd7863b0180fdc500d7e45f29d32738ed97
Daniel Roa <lion35528@gmail.com>
2022-10-19 23:40:13 -0500

    Add route contact

--- routes/router.js
+++ routes/router.js
@@ -6,19 +6,22 @@ const router = expres.Router();
 */
 
 router.get('/', (req, res, next) => {
-    res.render('index', { title: "Daniel Roa" })
+    res.render('index', { title: "Daniel Roa - Home" });
 })
 
 router.get('/skills', (req, res, next) => {
-    res.render('skills', { title: "Daniel Roa - Skills" })
+    res.render('skills', { title: "Daniel Roa - Skills" });
 })
 
 router.get('/about', (req, res, next) => {
-    res.send('Working on page about');
+    res.render('about', { title: "Daniel Roa - About" });
 })
 
 router.get('/education', (req, res, next) => {
     res.send('Working on page education');
 })
 
+router.get('/contact', (req, res, next) => {
+    res.render('contact', { title: "Daniel Roa - Contacto" });
+})
 module.exports = router;
